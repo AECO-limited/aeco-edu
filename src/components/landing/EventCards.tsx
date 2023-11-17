e3961b569e1e4b8fd15b7a8fefaaa40b7c86854f
oben-desmond <obend678@gmail.com>
2023-11-17 10:02:18 +0100

    changed images

--- src/components/landing/EventCards.tsx
+++ src/components/landing/EventCards.tsx
@@ -1,9 +1,10 @@
+import Image from 'next/image';
 import React from 'react'
 
 const events = [
 
     {
-        "image": "https://media.istockphoto.com/id/1175031702/photo/speaker-giving-presentation-on-scientific-business-conference.webp?b=1&s=170667a&w=0&k=20&c=iOOZK7OMaEA9nWKybGZKH9VRUiJLh1huctCWjySNTSg=",
+        "image": "/assets/event-1.webp",
         "date": {
             "month": "APR",
             "day": "14"
@@ -12,7 +13,7 @@ const events = [
         "description": "Unleash your creativity and explore the limitless possibilities of artistic expression. Join us for an immersive event that celebrates the power of art, music, and culture. Experience captivating performances, engage in interactive workshops, and connect with fellow artists from around the world. Discover new techniques, gain inspiration, and leave with a renewed passion for your craft. Ignite your creativity and let your imagination soar to new heights."
     },
     {
-        "image": "https://media.istockphoto.com/id/1186603900/photo/diverse-businesspeople-gathered-together-for-negotiations-lead-by-african-businesswoman.webp?b=1&s=170667a&w=0&k=20&c=0iPB7aIXPrFUbyJHC2QBOxRn8gr7XcG9ocF-AQeFMr8=",
+        "image": "/assets/event-2.webp",
         "date": {
             "month": "APR",
             "day": "14"
@@ -21,7 +22,7 @@ const events = [
         "description": "Join us for an empowering event dedicated to celebrating and supporting women in business. Gain valuable insights from successful female leaders who have shattered glass ceilings and overcome challenges. Learn strategies for personal and professional growth, network with like-minded individuals, and discover opportunities for collaboration. Together, let's break barriers, inspire change, and create a more inclusive and diverse business landscape."
     },
     {
-        "image": "https://www.mohawkcollege.ca/sites/default/files/International/Images/student-international-events.jpg",
+        "image": "/assets/event-3.jpeg",
         "date": {
             "month": "APR",
             "day": "14"
@@ -30,7 +31,7 @@ const events = [
         "description": "Take a break from the demands of daily life and embark on a transformative journey to prioritize your health and well-being. Join us for a rejuvenating retreat that combines mindfulness practices, physical activities, and expert guidance to help you achieve balance and inner harmony. Recharge your mind, body, and soul in a serene and supportive environment. Leave feeling refreshed, inspired, and equipped with tools to maintain a healthy lifestyle."
     },
     {
-        "image": "https://media.istockphoto.com/id/1426311994/photo/african-leader-manager-and-ceo-with-a-business-woman-in-the-office-with-her-team-in-the.webp?b=1&s=170667a&w=0&k=20&c=2Estd8U0KoANslWK0mrJUNZVJ7yJ9nORpB13rj_q62I=",
+        "image": "/assets/event-4.webp",
         "date": {
             "month": "APR",
             "day": "14"
@@ -51,7 +52,7 @@ const EventCards = () => {
                     events.map((event, index) => {
                         return (
                             <div className="shadow bg-white rounded-lg ">
-                                <img src={event.image} className='w-full rounded-t-lg' alt="" />
+                                <Image width={100} height={100} style={{ width: "100%", height: "auto" }} src={event.image} className='w-full rounded-t-lg' alt="" />
                                 <div className="px-6 py-10">
                                     <div className="flex gap-7">
                                         <section>
